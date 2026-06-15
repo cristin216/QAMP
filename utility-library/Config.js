410e2699b5c39cf578afab20ea0cfc2409d70792
cristin216 <cristinkalinowski@gmail.com>
2026-06-15 15:16:41 -0400

    added calendar/scheduling

--- utility-library/Config.js
+++ utility-library/Config.js
@@ -36,7 +36,14 @@ function setupScriptProperties() {
     'PROD_ROSTER_FOLDER_ID':                'your-id-here',
     'PROD_TEMPLATE_FOLDER_ID':              'your-id-here',
     'PROD_GENERATED_DOCUMENTS_FOLDER_ID':   'your-id-here',
-    'PROD_WEBAPP_URL':                      'your-url-here'
+    'PROD_WEBAPP_URL':                      'your-url-here',
+
+    // Scheduling (environment-independent)
+    'SCHEDULING_ID':                        'your-id-here',
+    'SCHEDULING_WEBAPP_URL':                'your-url-here',
+    'ROOM_1_CALENDAR_ID':                   'your-id-here',
+    'ROOM_2_CALENDAR_ID':                   'your-id-here',
+    'ROOM_3_CALENDAR_ID':                   'your-id-here'
   });
 
   Logger.log('Properties set. Delete the contents of this function now.');
@@ -44,6 +51,15 @@ function setupScriptProperties() {
 
 function getConfig() {
   var props = PropertiesService.getScriptProperties();
+
+  // Scheduling values are environment-independent
+  var scheduling = {
+    schedulingId:       props.getProperty('SCHEDULING_ID'),
+    schedulingWebAppUrl: props.getProperty('SCHEDULING_WEBAPP_URL'),
+    room1CalendarId:    props.getProperty('ROOM_1_CALENDAR_ID'),
+    room2CalendarId:    props.getProperty('ROOM_2_CALENDAR_ID'),
+    room3CalendarId:    props.getProperty('ROOM_3_CALENDAR_ID')
+  };
 
   return {
     test: {
@@ -57,7 +73,12 @@ function getConfig() {
       rosterFolderId:             props.getProperty('TEST_ROSTER_FOLDER_ID'),
       templateFolderId:           props.getProperty('TEST_TEMPLATE_FOLDER_ID'),
       generatedDocumentsFolderId: props.getProperty('TEST_GENERATED_DOCUMENTS_FOLDER_ID'),
-      webAppUrl:                  props.getProperty('TEST_WEBAPP_URL')
+      webAppUrl:                  props.getProperty('TEST_WEBAPP_URL'),
+      schedulingId:               scheduling.schedulingId,
+      schedulingWebAppUrl:        scheduling.schedulingWebAppUrl,
+      room1CalendarId:            scheduling.room1CalendarId,
+      room2CalendarId:            scheduling.room2CalendarId,
+      room3CalendarId:            scheduling.room3CalendarId
     },
     prod: {
       receiptsFolderId:           props.getProperty('PROD_RECEIPTS_FOLDER_ID'),
@@ -70,7 +91,12 @@ function getConfig() {
       rosterFolderId:             props.getProperty('PROD_ROSTER_FOLDER_ID'),
       templateFolderId:           props.getProperty('PROD_TEMPLATE_FOLDER_ID'),
       generatedDocumentsFolderId: props.getProperty('PROD_GENERATED_DOCUMENTS_FOLDER_ID'),
-      webAppUrl:                  props.getProperty('PROD_WEBAPP_URL')
+      webAppUrl:                  props.getProperty('PROD_WEBAPP_URL'),
+      schedulingId:               scheduling.schedulingId,
+      schedulingWebAppUrl:        scheduling.schedulingWebAppUrl,
+      room1CalendarId:            scheduling.room1CalendarId,
+      room2CalendarId:            scheduling.room2CalendarId,
+      room3CalendarId:            scheduling.room3CalendarId
     }
   };
 }
